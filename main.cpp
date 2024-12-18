265595409cd793b5e5e361659eafa8262f08120b
Shawn Halayka <sjhalayka@gmail.com>
2024-12-18 13:55:29 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,25 +4,50 @@ int main(void)
 {
 	srand(static_cast<unsigned int>(time(0)));
 
-	while (1)
+	map<short unsigned int, size_t> hand_class_counts;
+
+	size_t count = 1000000;
+
+	for(size_t i = 0; i < count; i++)
 	{
 		vector<card> deck;
 		init_deck(deck);
 		shuffle_cards(deck);
 
 		vector<card> hand;
-		deal_hand(deck, hand, 4);
 
-		short unsigned int c = get_best_wild_classification(hand, deck);
+		size_t num_init_cards = 5;
 
-		if (c == FULL_HOUSE)
-		{
-			print_hand_classification(c);
-			print_cards(hand);
-			cout << endl;
-		}
+		deal_hand(deck, hand, num_init_cards);
+
+		short unsigned int classification = 0;
+		
+		if (num_init_cards < 5)
+			classification = get_best_wild_classification(hand, deck);
+		else
+			classification = classify_hand(hand);
+
+		hand_class_counts[classification]++;
+
+
+		//if (c >= FULL_HOUSE)
+		//{
+		//	print_hand_classification(c);
+		//	print_cards(hand);
+		//	cout << endl;
+		//}
 	}
 
+
+	ofstream out_file("distribution.txt");
+
+	for (map<short unsigned int, size_t>::const_iterator ci = hand_class_counts.begin(); ci != hand_class_counts.end(); ci++)
+	{
+		print_hand_classification(ci->first);
+		cout << ci->second << endl << endl;
+
+		out_file << ci->first << " " << ci->second << endl;
+	}
 
 	//card c;
 	//hand.clear();
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <vector>
 #include <map>
